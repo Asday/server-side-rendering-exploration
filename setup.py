f7af5d5b7f15430e6d2347e8f4e298a6c823fdae
asday <sara.and.zuka@gmail.com>
2018-08-01 02:52:59 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+
+setup(
+    name='server_side_rendering_exploration',
+    version='0.0.0',
+    license='MIT',
+    description='What happens when we try to put react, django, and some manner of js server, in one repository?',
+    long_description='',
+    author='Adam Barnes',
+    author_email='sara.and.zuka@gmail.com',
+    url='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers  # noqa
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    install_requires=[
+    ],
+)
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+import server_side_rendering_exploration  # noqa
+
+
+def test_nothing():
+    assert 1 == 1
